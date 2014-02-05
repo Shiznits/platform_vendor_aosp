2c0361a173c60d25c968f4567a55f64c971b0ab0
Steve Kondik <shade@chemlab.org>
2014-02-04 22:59:01 -0800

    pure_phone.mk: persist.sys.dun.override=0

--- common.mk
+++ common.mk
@@ -165,4 +165,8 @@ ifneq ($(TARGET_BUILD_VARIANT),eng)
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
 endif
 
+# Thank you, please drive thru!
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.dun.override=0
+
 $(call inherit-product-if-exists, vendor/extra/product.mk)
